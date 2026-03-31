2f1a298e6f46d51298ec193b5dfd2862415d49de
aaditya poudel <aadityapoudel68@gmail.com>
2026-03-31 13:58:50 +0545

    Remove dotenv dependency and debug environment variables

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL environment variable is not set")
+    raise ValueError(f"DATABASE_URL environment variable is not set. Available env vars: {list(os.environ.keys())}")
 
 engine = create_engine(DATABASE_URL)
 
